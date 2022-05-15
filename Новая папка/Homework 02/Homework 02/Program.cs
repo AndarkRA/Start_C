3a19062f6dafe26bcdc699f8cec8e69beab27f99
AndarkRA <klushin1973@yandex.ru>
2022-05-15 20:07:58 +0300

    Task 02

--- Новая папка/Homework 02/Homework 02/Program.cs
+++ Новая папка/Homework 02/Homework 02/Program.cs
@@ -17,24 +17,107 @@ namespace Homework_02
             double maxTemp = double.Parse(Console.ReadLine());
 
             double midTemp;
-            midTemp = (minTemp + maxTemp) /2;
+            midTemp = (minTemp + maxTemp) / 2;
 
             Console.WriteLine($"Среднесуточная температура - {midTemp} градусов");
 
         }
 
+        /// <summary>
+        /// Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+        /// </summary>
+        static void Task02()
 
+        {
+            Console.Write("Введите номер месяца: ");
+            int numberMonth = int.Parse(Console.ReadLine());
+            string Month = "";
+            switch (numberMonth)
+            {
+                case 1: Month = "Январь";
+                    break;
+                case 2: Month = "Февраль";
+                    break ;
+                case 3:
+                    Month = "Март";
+                    break;
+                case 4:
+                    Month = "Апрель";
+                    break;
+                case 5:
+                    Month = "Май";
+                    break;
+                case 6:
+                    Month = "Июнь";
+                    break;
+                case 7:
+                    Month = "Июль";
+                    break;
+                case 8:
+                    Month = "Август";
+                    break;
+                case 9:
+                    Month = "Сентябрь";
+                    break;
+                case 10:
+                    Month = "Октябрь";
+                    break;
+                case 11:
+                    Month = "Ноябрь";
+                    break;
+                case 12:
+                    Month = "Декабрь";
+                    break;
+                    default:
+                    Console.WriteLine("Укажите номер месяца от 1 до 12");
+                    break;
 
+            }
+            Console.WriteLine(Month);
 
-
-
-
-
+        }
+ 
 
 
         static void Main(string[] args)
         {
-            Task01();
+
+            Console.WriteLine("Домашнее задание к уроку 2");
+            Console.WriteLine("======================");
+            Console.WriteLine("1 -> Задача 1");
+            Console.WriteLine("2 -> Задача 2");
+            Console.WriteLine("0 -> Заврешение работы приложения");
+            Console.WriteLine("======================");
+
+            Console.Write("Введите номер задачи ");
+            int numberTask = int.Parse(Console.ReadLine());
+
+            switch (numberTask)
+            {
+                case 1:
+                    Task01();
+                    break;
+
+                case 2:
+                    Task02();
+                    break;
+
+
+
+                case 0:
+                    Console.WriteLine("Завершение работы приложения ...");
+                    Console.ReadKey();
+                    return;
+
+                default:
+                    Console.WriteLine("Некорректный номер задачи, \nПовторите попытку ввода... ");
+                    Console.ReadKey();
+                    break;
+
+
+                    Task01();
+                    Task02();
+            }
         }
     }
 }
